7602431adee92df38c82f0ee10322c4c4cf8e651
Aryan Malhotra <maddyyy467@gmail.com>
2024-10-29 01:03:05 -0400

    Completed Maschine Learning Linear Regression model

--- LinearRegression.jl
+++ LinearRegression.jl
@@ -17,7 +17,7 @@ p = scatter(X, Y,
     xlabel = "Size (sqft)",
     ylabel = "Price (thousands of dollars)",
     title = "Housing Prices in Portland",
-    legend = false,
+    # legend = false,
     color = :red
 )
 
@@ -25,13 +25,13 @@ p = scatter(X, Y,
 
 ols = lm(@formula(y ~ x), t)
 
-plot!(X, predict(ols), color=:green, linewidth=3)
+plot!(X, predict(ols), color=:green, linewidth=3, label="Analytical Solution")
 
 #########################################
 # Machine Learning Approach
 #########################################
 
-epochs = 0
+epochs = 0 #just means the count of iterations completed while optimising and minimising the cost function
 
 p = scatter(X, Y,
     xlims = (0, 5000),
@@ -39,7 +39,7 @@ p = scatter(X, Y,
     xlabel = "Size (sqft)",
     ylabel = "Price (thousands of dollars)",
     title = "Housing Prices in Portland (epochs = $epochs)",
-    legend = false,
+    # legend = false,
     color = :red
 )
 
@@ -58,8 +58,73 @@ h(x) = θ_0 .+ θ_1*x #This is what we hypothesis the function to look like
 
 N = length(X)
 ypred = ŷ = h(X)
-J(θ_0, θ_1) = (1/(2N)) * sum((ŷ - Y).^2)
+J(X, Y) = (1/(2N)) * sum((ŷ - Y).^2)
 
 J_history = []
 
-push!(J_history, J(θ_0, θ_1))
+push!(J_history, J(X,Y))
+
+#How to minimise the cost function
+
+#Gradient Descent Algorithm 
+
+#To minimise the cost function, you need dJ/dt to be zero.
+# i.e. you need to find the root of the differential of the Cost function
+# like newton's method of finding a root (x_n+1 = x_n - f(x)/f'(x)) basically, setting the descent rate to be determined by the gradient to fall into the pit of a zero
+# we define a very loosely similar expression (x_n+1 = x_n - α f(x)) where alpha is controllable (f(x) here is the derivative of the function since we want ITs root)
+# we do this for partial derivative wrt both the variables since we like to control them individually
+
+pd_θ_0(X,Y) = (1/(N)) * sum((ŷ - Y))
+pd_θ_1(X,Y) = (1/(N)) * sum((ŷ - Y).*X)
+
+α_0 = 0.09 #This regression rate or the LEARNING rate is for the bias
+α_1 = 8.0e-8 # Learning rate for the slope
+
+
+# Using the Algorithm to Optimise Iteratively (mannual iteration)
+θ_0_temp = pd_θ_0(X,Y)
+θ_1_temp = pd_θ_1(X,Y)
+
+# adjusting the parameters based off of partial derivatives
+θ_0 -= α_0 * θ_0_temp
+θ_1 -= α_1 * θ_1_temp
+
+
+ŷ = h(X)
+push!(J_history, J(X, Y))
+#The value of the cost function dropped from 65k to 21k! Huge improvement!
+
+epochs += 1
+
+plot!(X, ŷ, color = :blue, alpha = 0.5,
+    title = "House Prices in Portland (epochs = $epochs)"
+)
+
+# using the gradient Descent Algorithm (with a loop)
+
+for i in 1:100
+    θ_0_temp = pd_θ_0(X,Y)
+    θ_1_temp = pd_θ_1(X,Y)
+
+    # adjusting the parameters based off of partial derivatives
+    θ_0 -= α_0 * θ_0_temp
+    θ_1 -= α_1 * θ_1_temp
+
+
+    ŷ = h(X)
+    push!(J_history, J(X, Y))
+    #The value of the cost function dropped from 65k to 21k! Huge improvement!
+
+    epochs += 1
+
+    plot!(X, ŷ, color = :blue, alpha = 0.5, label=nothing,
+        title = "House Prices in Portland (epochs = $epochs)"
+    )
+    display(p)
+
+    yield()
+    sleep(0.1)
+end
+
+
+display(plot)
